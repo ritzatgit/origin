d4af37f3c1a3a3b9c7cc2789c1182de602747784
ritz <ritztechs@gmail.com>
2022-06-05 17:31:05 +0000

    first.java modified here at master

--- first.java
+++ first.java
@@ -1,2 +1,2 @@
 The first file i am creating in local server
-the modification i am doing @ master branch and again i am writing it on the same line @ bug1  
+the modification i am doing @ master branch  
